d0110d66adc904bb5e5974a551283fa12e6f4df0
RebelLion420 <gaigecarlos@gmail.com>
2020-06-08 07:09:24 -0700

    200608 update include 'lz4'
    Signed-off-by: RebelLion420 <gaigecarlos@gmail.com>

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -1,3 +1,3 @@
-echo "RUN apt-get install -y sudo bc ccache git gnupg flex bison build-essential zip curl gcc-multilib g++-multilib liblz4-dev"
+echo "RUN apt-get install -y sudo bc ccache git gnupg flex bison build-essential zip curl gcc-multilib g++-multilib liblz4-tool"
 
 echo "RUN mkdir /root/toolchain && curl -L https://releases.linaro.org/components/toolchain/binaries/latest-4/arm-linux-gnueabi/gcc-linaro-4.9.4-2017.01-x86_64_arm-linux-gnueabi.tar.xz | tar xJ --strip-components=1 -C /root/toolchain/"
